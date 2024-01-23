e297e11d12e2966b612bfa9d541b014874a574ae
mosyocoder <128539219+mosyocoder@users.noreply.github.com>
2024-01-23 17:30:40 +0300

    header and nav

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import "./style.css";
+import avatar from "../../assets/enes_avatar.jpg";
+
+function Navigation() {
+	return (
+		<div className="header">
+			<div>
+				<img src={avatar} alt="" />
+				<h1>Enes Seval</h1>
+				<div className="social-links">
+					<a href="https://twitter.com/mosyocoder" target="_blank" rel="noopener noreferrer">
+						<i class="fa-brands fa-x-twitter"></i>
+					</a>
+					<a href="https://www.instagram.com/mosyocoder/" target="_blank" rel="noopener noreferrer">
+						<i class="fa-brands fa-instagram"></i>
+					</a>
+					<a href="https://www.linkedin.com/in/enes-seval-162679230/" target="_blank" rel="noopener noreferrer">
+						<i class="fa-brands fa-linkedin-in"></i>
+					</a>
+					<a href="https://github.com/mosyocoder" target="_blank" rel="noopener noreferrer">
+						<i class="fa-brands fa-github"></i>
+					</a>
+				</div>
+			</div>
+			<div className="nav-menu">
+				<ul>
+					<li>
+						<a href="/">
+							<i class="fa-solid fa-house fa-2x"></i>
+							<span>Home</span>
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<i class="fa-solid fa-user fa-2x"></i>
+							<span>About</span>
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<i class="fa-solid fa-file-code fa-2x"></i>
+							<span>Portfolio</span>
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<i class="fa-solid fa-envelope fa-2x"></i>
+							<span>Contact</span>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+export default Navigation;
