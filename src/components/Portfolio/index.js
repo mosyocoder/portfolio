5ce9692fde6e24970b23db4723f3013da75a70b4
mosyocoder <128539219+mosyocoder@users.noreply.github.com>
2024-03-01 17:57:03 +0300

    portfolio section add projects images

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./style.css";
 import bmi from "../../assets/bmiss.png";
+import anonymous from "../../assets/anonchatss.png";
+import ecom from "../../assets/ecommercess.png";
+import notes from "../../assets/noteappss.png";
+import vote from "../../assets/voteappss.png";
+import weather from "../../assets/weather.png";
 
 function Portfolio() {
+	const [isHovered, setIsHovered] = useState(false);
+	const Card = ({ imgSrc, sourceCodeLink, liveLink }) => {
+		return (
+			<div className="card" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+				<img src={imgSrc} alt="" />
+				{isHovered && (
+					<div className="overlay">
+						<a href={sourceCodeLink} rel="noreferrer" className="source-code" target="_blank">
+							Kaynak Kodu
+						</a>
+						<a href={liveLink} rel="noreferrer" className="live-link" target="_blank">
+							Canlı Link
+						</a>
+					</div>
+				)}
+			</div>
+		);
+	};
 	return (
 		<section className="portfolio" data-aos="slide-right">
 			<div className="container">
@@ -11,16 +34,12 @@ function Portfolio() {
 					<h2>Portfolio</h2>
 				</div>
 				<div className="content">
-					<div
-						style={{
-							backgroundImage: `url(${bmi})`,
-						}}
-						className="card"></div>
-					<div className="card"></div>
-					<div className="card"></div>
-					<div className="card"></div>
-					<div className="card"></div>
-					<div className="card"></div>
+					<Card imgSrc={bmi} sourceCodeLink="#" liveLink="#" />
+					<Card imgSrc={anonymous} sourceCodeLink="#" liveLink="#" />
+					<Card imgSrc={ecom} sourceCodeLink="#" liveLink="#" />
+					<Card imgSrc={notes} sourceCodeLink="#" liveLink="#" />
+					<Card imgSrc={vote} sourceCodeLink="#" liveLink="#" />
+					<Card imgSrc={weather} sourceCodeLink="#" liveLink="#" />
 				</div>
 			</div>
 		</section>
